fd62b5e2bebd26ffaa156e8deb60bce3000ae43b
Abdelaziz Salah <abdelaziz132001@gmail.com>
2022-09-11 14:49:33 +0200

    finalizing all azkars

--- lib/Widgets/AzkarElWodo2.dart
+++ lib/Widgets/AzkarElWodo2.dart
@@ -1,12 +1,37 @@
+import 'package:azkark/Widgets/AzkarContainer.dart';
 import 'package:flutter/material.dart';
 
 class AzkarElWodo2 extends StatelessWidget {
-  const AzkarElWodo2({super.key});
-
+  List<Map> azkary = [
+    {
+      'zekr': '''
+قبل الوضوء: "بِسْمِ ٱللّٰهِ"''',
+      'counter': 1,
+      'initial': 1
+    },
+    {
+      'zekr': '''
+"سُبْحَانَكَ اللَّهُمَّ وبَحَمْدكَ أشْهدُ أنْ لا إلهَ إلا أنْتَ أَسْتَغْفِرُكَ وأتُوبُ إِلَيْكَ".''',
+      'counter': 1,
+      'initial': 1
+    },
+    {
+      'zekr': '''
+"اللَّهُمَّ اجْعَلْني مِنَ التَّوَّابينَ واجْعَلْنِي من المُتَطَهِّرِينَ".
+''',
+      'counter': 1,
+      'initial': 1
+    },
+    {
+      'zekr': '''
+"أشْهَدُ أن لا إله إلا الله وحْدَهُ لا شريكَ لهُ ، وأشْهَدُ أنَّ محمداً عَبدُهُ ورسُولُه".
+''',
+      'counter': 1,
+      'initial': 1
+    },
+  ];
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Card(child: Text("Azkar El Wodo2")),
-    );
+    return AzkarContainer(azkary: azkary);
   }
 }
