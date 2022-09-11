cc8aefc458aa30116c03755aba1135d68f4726f2
Ahmed Lotfy <76037906+AhmedLotfy02@users.noreply.github.com>
2022-09-11 10:34:09 +0200

    اذكار الاستيقاظ

--- lib/Widgets/AzkarB3dElSalah.dart
+++ lib/Widgets/AzkarB3dElSalah.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/material.dart';
 import 'Zekr.dart';
 
 class AzkarB3dElSalah extends StatefulWidget {
--- lib/Widgets/AzkarElEstykaz.dart
+++ lib/Widgets/AzkarElEstykaz.dart
@@ -1,12 +1,62 @@
 import 'package:flutter/material.dart';
+import 'Zekr.dart';
 
-class AzkarElEstykaz extends StatelessWidget {
-  const AzkarElEstykaz({super.key});
+class AzkarElEstykaz extends StatefulWidget {
+  @override
+  State<AzkarElEstykaz> createState() => _AzkarElEstykazState();
+}
+
+class _AzkarElEstykazState extends State<AzkarElEstykaz> {
+  List<Map<String, Object>> azkary = [
+    {
+      'zekr':
+      '  َالحَمْـدُ لِلّهِ الّذي أَحْـيانا بَعْـدَ ما أَماتَـنا وَإليه النُّـشور. '
+      ,'counter': 1,
+      'initial': 1
+    },
+    {
+      'zekr':
+      'الحمدُ للهِ الذي عافاني في جَسَدي وَرَدّ عَليّ روحي وَأَذِنَ لي بِذِكْرِه . ',
+      'counter': 1,
+      'initial': 1
+    },
+    {
+      'zekr':
+      'لا إلهَ إلاّ اللّهُ وَحْـدَهُ لا شَـريكَ له، لهُ المُلـكُ ولهُ الحَمـد، وهوَ على كلّ شيءٍ قدير، سُـبْحانَ اللهِ، والحمْـدُ لله ، ولا إلهَ إلاّ اللهُ واللهُ أكبَر، وَلا حَولَ وَلا قوّة إلاّ باللّهِ العليّ العظيم. رَبِّ اغْفرْ لي  .',
+      'counter': 1,
+      'initial': 1
+    },
+
+
+  ];
 
   @override
   Widget build(BuildContext context) {
+    int couter = 0;
     return Container(
-      child: Card(child: Text("Azkar El Estykaz")),
-    );
+        color: Colors.green[300],
+        child: Card(
+            child: ListView.builder(
+              itemBuilder: (context, index) {
+                return Zekr(
+                  counter: azkary[index]['counter'],
+                  zekr: azkary[index]['zekr'],
+                  decrementCounter: () {
+                    setState(() {
+                      if (azkary[index]['counter'] != 0)
+                        azkary[index]['counter'] =
+                            (azkary[index]['counter'] as int) - 1;
+                    });
+                  },
+                  resetCounter: () {
+                    setState(() {
+                      azkary[index]['counter'] = azkary[index]['initial'] as int;
+                    });
+                  },
+                );
+              },
+              itemCount: azkary.length,
+            )));
   }
 }
+
