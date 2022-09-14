4f6aefbebf26130f07bc152dbbe387237503d4bd
Ahmed Lotfy <76037906+AhmedLotfy02@users.noreply.github.com>
2022-09-14 10:15:35 +0200

    dua2 choices text direction

--- lib/Screens/choice_doa2Widget.dart
+++ lib/Screens/choice_doa2Widget.dart
@@ -1,3 +1,4 @@
+import 'package:azkark/Screens/HomePage.dart';
 import 'package:azkark/controllers/service.dart';
 import 'package:flutter/material.dart';
 
@@ -10,33 +11,36 @@ class choice_doa2Widget extends StatelessWidget {
   choice_doa2Widget(this.image, this.word, this.choice,this.darkMode,this.darkModeSetter);
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      children: [
-        ClipRRect(
-          borderRadius: BorderRadius.circular(15),
-          child: Image.asset(
-            image,
-            height: MediaQuery.of(context).size.height,
-            fit: BoxFit.cover,
-          ),
-        ),
-        GestureDetector(
-          onTap: () {
-            chooseOptioninDoa2Page(context, choice,darkMode,darkModeSetter);
-          },
-          child: Container(
-            padding: EdgeInsets.all(10),
-            alignment: Alignment.center,
-            child: Text(
-              word,
-              style: TextStyle(fontSize: 30, color: Colors.white),
+    return Directionality(
+      textDirection: HomePage.languageChoice==false?TextDirection.rtl:TextDirection.ltr,
+      child: Stack(
+        children: [
+          ClipRRect(
+            borderRadius: BorderRadius.circular(15),
+            child: Image.asset(
+              image,
+              height: MediaQuery.of(context).size.height,
+              fit: BoxFit.cover,
             ),
-            decoration: BoxDecoration(
-                color: Colors.black.withOpacity(0.4),
-                borderRadius: BorderRadius.circular(15)),
           ),
-        )
-      ],
+          GestureDetector(
+            onTap: () {
+              chooseOptioninDoa2Page(context, choice,darkMode,darkModeSetter);
+            },
+            child: Container(
+              padding: EdgeInsets.all(10),
+              alignment: Alignment.center,
+              child: Text(
+                word,
+                style: TextStyle(fontSize: 28, color: Colors.white),
+              ),
+              decoration: BoxDecoration(
+                  color: Colors.black.withOpacity(0.4),
+                  borderRadius: BorderRadius.circular(15)),
+            ),
+          )
+        ],
+      ),
     );
   }
 }
